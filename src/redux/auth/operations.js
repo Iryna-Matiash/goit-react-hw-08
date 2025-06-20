771d7343ec2bd37305ccc0c51f8cd0b9b563f07f
Iryna-Matiash <165329434+Iryna-Matiash@users.noreply.github.com>
2025-06-20 15:09:43 +0300

    update

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,47 +1,53 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+
 axios.defaults.baseURL = "https://connections-api.goit.global";
 
-const setAuthHeader = (value: string) => {
-  axios.defaults.headers.common.Authorization = value;
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = token;
 };
+
 
 const clearAuthHeader = () => {
   delete axios.defaults.headers.common.Authorization;
 };
 
+
 export const register = createAsyncThunk(
   "auth/register",
-  async (credentials: { name: string; email: string; password: string }, thunkAPI) => {
+  async (credentials, thunkAPI) => {
     try {
       const response = await axios.post("/users/signup", credentials);
       setAuthHeader(`Bearer ${response.data.token}`);
       return response.data;
-    } catch (error: any) {
+    } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
+
 export const logIn = createAsyncThunk(
   "auth/login",
-  async (credentials: { email: string; password: string }, thunkAPI) => {
+  async (credentials, thunkAPI) => {
     try {
       const response = await axios.post("/users/login", credentials);
       setAuthHeader(`Bearer ${response.data.token}`);
       return response.data;
-    } catch (error: any) {
+    } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
+
 
 export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
     await axios.post("/users/logout");
     clearAuthHeader(); 
-  } catch (error: any) {
+  } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
 });
@@ -51,24 +57,25 @@ export const refreshUser = createAsyncThunk(
   "auth/refresh",
   async (_, thunkAPI) => {
     try {
-      const state: any = thunkAPI.getState();
+      const state = thunkAPI.getState();
       const token = state.auth.token;
 
       if (!token) {
-        return thunkAPI.rejectWithValue("No valid token");
+        return thunkAPI.rejectWithValue("Користувач не авторизований");
       }
 
       setAuthHeader(`Bearer ${token}`);
       const response = await axios.get("/users/current");
       return response.data;
-    } catch (error: any) {
+    } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   },
   {
     condition: (_, thunkAPI) => {
-      const state: any = thunkAPI.getState();
+      const state = thunkAPI.getState();
       return state.auth.token !== null;
     },
   }
 );
+
